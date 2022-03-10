a953dd8e82191c074e2e89e5d41a23b887f18c25
LeeWoongPyo <44749633+KOR-BanBaZy@users.noreply.github.com>
2022-03-10 15:00:12 +0900

    앙 기모띠. 첫 코딩 올림
    기분이 조오타~

---
+++ syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');       //이항연산자(산술연산자가 아님)
+console.log('앙 기모띠'.length);
---
+++ syntax/variable2.js
@@ -0,0 +1,4 @@
+var name = '앙';
+var letter = 'Dear '+name+' 앙 기모띠'+name+'하구만!!'+name;
+
+console.log(letter);
